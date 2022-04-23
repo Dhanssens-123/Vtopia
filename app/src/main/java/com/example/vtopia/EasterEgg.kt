f83b3ba65097ed9b768cee98600a8eff93cc453a
DPJulien <julien.depraetere@gmail.com>
2022-04-23 21:28:00 +0200

    ajout de l'écran d'accueil v1

---
+++ app/src/main/java/com/example/vtopia/EasterEgg.kt
@@ -0,0 +1,11 @@
+package com.example.vtopia
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EasterEgg : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_easter_egg)
+    }
+}
--- app/src/main/java/com/example/vtopia/WelcomeScreen.kt
+++ app/src/main/java/com/example/vtopia/WelcomeScreen.kt
@@ -2,18 +2,48 @@ package com.example.vtopia
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
-class WelcomeScreen : AppCompatActivity() {
+class WelcomeScreen() : AppCompatActivity(), Parcelable {
+    constructor(parcel: Parcel) : this() {
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome_screen)
     }
-    fun newGame(view : View) {
+
+    fun newGame(view: View) {
         // Commence une nouvelle partie quand le bouton est pressé
-        val intent = Intent(this, MainActivity::class.java).apply {
+        startActivity(Intent(this, MainActivity::class.java).apply {})
+    }
+
+    fun tips(view: View) {
+        // Montre les règles du jeu
+        startActivity(Intent(this, Tips::class.java).apply {})
+        fun easterEgg(view: View) {
+            startActivity(Intent(this, EasterEgg::class.java).apply {})
         }
-        startActivity(intent)
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<WelcomeScreen> {
+        override fun createFromParcel(parcel: Parcel): WelcomeScreen {
+            return WelcomeScreen(parcel)
+        }
+
+        override fun newArray(size: Int): Array<WelcomeScreen?> {
+            return arrayOfNulls(size)
+        }
     }
 }
